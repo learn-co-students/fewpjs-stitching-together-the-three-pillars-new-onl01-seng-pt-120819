85f7e6322de14453236e45cf7601339e9874d779
jamesabenj <james.abenj@gmail.com>
2020-06-18 11:40:48 -0400

    Done.

--- demo.js
+++ demo.js
@@ -10,24 +10,24 @@ let colorStates = {
   "": "red"
 };
 
-// STEP 1: This code is what lets JavaScript find the elements that we want to make clickable.
-// Without JavaScript, clicking on these heart shapes does nothing. Uncomment
-// this code and refresh the demo page.
+ //STEP 1: This code is what lets JavaScript find the elements that we want to make clickable.
+ //Without JavaScript, clicking on these heart shapes does nothing. Uncomment
+ //this code and refresh the demo page.
 
-// let articleHearts = document.querySelectorAll(".like-glyph");
+let articleHearts = document.querySelectorAll(".like-glyph");
 
 function likeCallback(e) {
   let heart = e.target;
   mimicServerCall()
     .then(function(serverMessage){
-      // STEP 2: Uncomment the next 3 lines.
-      // We'll use Pillar 1 (DOM Manipulation) to update the screen and
-      // mimic Pillar 3 (Server Communication) to only update the screen if the
-      // sending of information to the server succeeds.
+      //  STEP 2: Uncomment the next 3 lines.
+      //  We'll use Pillar 1 (DOM Manipulation) to update the screen and
+      //  mimic Pillar 3 (Server Communication) to only update the screen if the
+      //  sending of information to the server succeeds.
       alert("You notified the server!");
-      // alert(serverMessage);
-      // heart.innerText = glyphStates[heart.innerText];
-      // heart.style.color = colorStates[heart.style.color];
+      alert(serverMessage);
+      heart.innerText = glyphStates[heart.innerText];
+      heart.style.color = colorStates[heart.style.color];
     })
     .catch(function(error) {
       alert("Something went wrong!");
@@ -39,9 +39,9 @@ function likeCallback(e) {
 // code when an "event" is fired. That's Pillar 2, event handling. Uncomment
 // this code.
 
-//for (let glyph of articleHearts) {
-  //glyph.addEventListener("click", likeCallback);
-//}
+for (let glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
 
 // STEP 4: 
 
@@ -54,10 +54,10 @@ function likeCallback(e) {
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
 
-function mimicServerCall() {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      resolve("Pretend remote server notified of action!");
-    }, 300);
-  });
-}
+// function mimicServerCall() {
+//   return new Promise(function(resolve, reject) {
+//     setTimeout(function() {
+//       resolve("Pretend remote server notified of action!");
+//     }, 300);
+//   });
+// }
